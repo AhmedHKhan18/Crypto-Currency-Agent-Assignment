8f8f7456bd5fa44710f5cb5e0fd245e2c71510a5
AhmedHKhan18 <ahmed.18hassan.18@gmail.com>
2025-07-25 10:43:25 +0500

    more enhanced crypto agent

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
 from agents import Agent, Runner, function_tool
 from connection import config
 import requests
+import chainlit as cl
 
 @function_tool
-def crypto_currency() -> str:
-    url = 'https://api.binance.com/api/v3/ticker/price?symbol={currency}'
+def crypto_currency(symbol) -> str:
+    url = f'https://api.binance.com/api/v3/ticker/price?symbol={symbol.upper()}'
     response = requests.get(url)
 
     if response.status_code == 200:
         return response.json()
     else:
         return {"error": f"Failed to fetch data, status code: {response.status_code}"}
+@cl.on_message
+async def main(input: cl.Message):
+    user_input = input.content
 
-agent = Agent(
-    name = 'Current Data',
-    instructions = 
-    """
-        You are an helpful assistant. Your task is to
-        help user with its queries.
-    """,
-    tools = [crypto_currency]
-)
+    agent = Agent(
+        name = 'Crypto Data Provider',
+        instructions = 
+        """
+            You are an crypto currency data provider.your task is to provide data about all crypto currencies.
+        """,
+        tools = [crypto_currency]
+    )
 
-result = Runner.run_sync(
-    agent, 
-    input = 'can you convert BTCUSDT',
-    run_config=config
-)
+    result = await Runner.run(
+        agent, 
+        input = user_input,
+        run_config=config
+    )
+    
+    await cl.Message(
+        content=f"{result.final_output}",
+    ).send()
 
-print(result.final_output)
+if __name__ == "__main__":
+    main()
